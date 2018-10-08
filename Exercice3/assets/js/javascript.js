53257d73d601dc700baeca7354548e1dfca2bf58
FabienCOTE <fabien.cote@gmail.com>
2018-10-08 11:42:09 +0200

    Correction javascript exercice 3

--- Exercice3/assets/js/javascript.js
+++ Exercice3/assets/js/javascript.js
@@ -1,12 +1,12 @@
 var valid = 0;
 
 while(valid != 1) {
-  var regexName = /^[a-zA-Z-]/;
+  var regexName = /^[a-zA-ZáàâäãåçéèêëíìîïñóòôöõúùûüýÿæœÁÀÂÄÃÅÇÉÈÊËÍÌÎÏÑÓÒÔÖÕÚÙÛÜÝŸ][a-záàâäãåçéèêëíìîïñóòôöõúùûüýÿæœÁÀÂÄÃÅÇÉÈÊËÍÌÎÏÑÓÒÔÖÕÚÙÛÜÝŸ]+([-'\s][a-zA-ZáàâäãåçéèêëíìîïñóòôöõúùûüýÿæœÁÀÂÄÃÅÇÉÈÊËÍÌÎÏÑÓÒÔÖÕÚÙÛÜÝŸ][a-záàâäãåçéèêëíìîïñóòôöõúùûüýÿæœÁÀÂÄÃÅÇÉÈÊËÍÌÎÏÑÓÒÔÖÕÚÙÛÜÝŸ]+)?$/
   var insertName = prompt("Saisir votre prénom");
   if(insertName == null || insertName.length == 0) {
     alert("Erreur, veuillez recommencer")
   }
-  else if (regexName.test(insertName)) {
+  else if (regexName.test(insertName) == true) {
     alert("Bonjour " + insertName);
     var valid = 1;
   }
